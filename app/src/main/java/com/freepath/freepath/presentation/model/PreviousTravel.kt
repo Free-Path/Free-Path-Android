f445ba9bae131b45df9cc7fe5b7d5bdffe6d18e3
JIEUNI <jieundaeun@naver.com>
2024-10-18 00:33:53 +0900

    [feat] #5 여행 메인 뷰 - 필요한 Entity 추가

--- app/src/main/java/com/freepath/freepath/presentation/model/PreviousTravel.kt
+++ app/src/main/java/com/freepath/freepath/presentation/model/PreviousTravel.kt
@@ -1,8 +1,7 @@
 package com.freepath.freepath.presentation.model
 
-data class UpcomingTravel(
+data class PreviousTravel(
     val upcomingTravelImg: String,
-    val upcomingTravelDday: String,
     val upcomingTravelName: String,
     val upcomingTravelPeriod: String
 )
