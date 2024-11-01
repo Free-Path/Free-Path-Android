e64a23927a8e9af923f28ad4e73d9b6c13cb669c
ChoiUS <chldn323@gmail.com>
2024-11-02 01:08:43 +0900

    feat: PlanDetail 변수 타입 변경

--- app/src/main/java/com/freepath/freepath/presentation/model/Plan.kt
+++ app/src/main/java/com/freepath/freepath/presentation/model/Plan.kt
@@ -4,11 +4,17 @@ import java.time.LocalDate
 
 data class Plan(val planDates: List<PlanDate>) {
     val dates = planDates.map(PlanDate::date)
+    val details = planDates.flatMap(PlanDate::planDetails)
 }
 
 val planEx = Plan(List(5) { day ->
     PlanDate(
         LocalDate.now().plusDays(1L + day),
-        List(3) { index -> planDetailEx.copy(id = day * 100 + index) }
+        List(3) { index ->
+            planDetailEx.copy(
+                id = day * 100 + index,
+                latLng = planDetailEx.latLng.offset(50.0 * index * day, 30.0 * index * day)
+            )
+        }
     )
 })
